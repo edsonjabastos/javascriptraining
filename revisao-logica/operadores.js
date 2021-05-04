c6d861755691e194695f03508d42a2842157653a
edsonjabastos <edsonjabastos@hotmail.com>
2021-05-04 17:32:46 -0300

    operadores de comparação

---
+++ revisao-logica/operadores.js
@@ -0,0 +1,72 @@
+// + (MAIS), - (MENOS), * (MULTIPLICAÇÃO), / (DIVISÃO),
+// % (MOD OU RESTO DA DIVISÃO), ** (POTENCIAÇÃO)
+let _10 = 10
+let _20 = 20
+let _5 = 5
+_5 = _5 + 5
+console.log(_5)
+_5 = _5 - 5
+console.log(_5)
+_5 = _5 * 5
+console.log(_5)
+_5 = _5 / 5
+console.log(_5)
+_5 = _5 % 5
+console.log(_5)
+_5 = _5 ** 5
+console.log(_5)
+console.log(_10 + _20)
+console.log(_10 - _20)
+console.log(_10 * _20)
+console.log(_10 / _20)
+console.log(_10 % _20)
+console.log(_10 ** _20)
+// OPERADORES DE ATRIBUIÇÃO
+let _11 = 10
+_11 += 2
+console.log(_11)
+let _12 = 10
+_12 -= 2
+console.log(_12)
+let _13 = 10
+_13 %= 2
+console.log(_13)
+let _14 = 10
+_14 *= 2
+console.log(_14)
+let _15 = 10
+_15 /= 2
+console.log(_15)
+let _16 = 10
+_16 **= 2
+console.log(_16)
+// INCREMENTO E DECREMENTO
+let i = 0
+i++
+console.log(i)
+i = 0
+console.log(++i)
+i = 0
+console.log(--i)
+i = 0
+console.log(i++)
+console.log(i)
+i = 0
+console.log(++i, --i, i++, i--)
+console.log(i)
+i = 0
+--i
+console.log(i)
+// COMPARAÇÃO
+// igualdade de valor == igualdade de valor e tipo ===
+// < (MENOR), > (MAIOR), <= (MENOR OU IGUAL), >= (MAIOR OU IGUAL)
+// != valores diferentes !== valores e tipos diferentes
+let v1 = 10
+let v2 = "10"
+let v3 = 11
+console.log(v1 == 10, v1 == v2, v1 == v3)
+console.log(v1 === 10, v1 === v2, v1 === v3)
+console.log(v1 <=10, v1 <= v2, v1 <= v3)
+console.log(v1 >= 10, v1 >= v2, v1 >= v3)
+console.log(v1 != 10, v1 != v2, v1 != v3)
+console.log(v1 !== 10, v1 !== v2, v1 !== v3)
