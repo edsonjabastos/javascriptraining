ca6a93317aca7201273e6f2c1b496863d1f9e576
edsonjabastos <edsonjabastos@hotmail.com>
2021-06-29 10:10:41 -0300

    Create async_await_1.js

---
+++ coder/ESnext/async_await_1.js
@@ -0,0 +1,8 @@
+function eperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log('Executando promise...')
+            resolve()
+        }, tempo)
+    })
+}
