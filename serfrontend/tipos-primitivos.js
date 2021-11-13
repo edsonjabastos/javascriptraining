e53aab2a1c4a88baafe3b5f9e48e1a216956f192
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-11-13 14:38:59 -0300

    variable and types

---
+++ serfrontend/tipos-primitivos.js
@@ -0,0 +1,29 @@
+console.log(`\` --- '' --- ""`)
+console.log("\" --- '' --- `")
+console.log('\' --- "" --- ``')
+
+let idade = -100
+let msg = "eu nem nasci pois tenho " + idade + " ânus"
+console.log(msg)
+let _age = -598176396
+let _msg = `eu tenho ${_age} então eu vim no big bang`
+console.log(`o tipo da _msg é ${typeof _msg} e seu valor é ${_msg}`)
+
+let n1 = 10
+let n2 = 1.1
+console.log(`o tipo do n1 é ${typeof n1} e seu valor é ${n1}`)
+console.log(`o tipo do n2 é ${typeof n2} e seu valor é ${n2}`)
+
+let george = true
+let boole = false
+console.log(`o tipo do george é ${typeof george} e seu valor é ${george}`)
+console.log(`o tipo do boole é ${typeof boole} e seu valor é ${boole}`)
+
+let nula
+console.log(nula, typeof nula)
+nula = undefined
+console.log(nula, typeof nula)
+let _nula = null
+console.log(_nula, typeof _nula)
+_nula = 10
+console.log(_nula, typeof _nula)
