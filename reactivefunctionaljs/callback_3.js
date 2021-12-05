1909b28a1b19cf0a04aac187e82b97b861bfdb0d
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-05 10:43:17 -0300

    Função Map#3

--- reactivefunctionaljs/callback_3.js
+++ reactivefunctionaljs/callback_3.js
@@ -8,3 +8,19 @@ const primeiraLetra = texto => texto[0]
 const letras = nomes.map(primeiraLetra)
 console.log(nomes, letras)
 // console.log(nomes.map(primeiraLetra))
+
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.99 },
+    { nome: 'Impressora', qtde: 0, preco: 649.50 },
+    { nome: 'Caderno', qtde: 4, preco: 27.10 },
+    { nome: 'Lapis', qtde: 3, preco: 5.82 },
+    { nome: 'Tesoura', qtde: 1, preco: 19.20 },
+]
+
+const nomesItems = obj1 => obj1.nome
+const nomeItemsCarrinho = carrinho.map(nomesItems)
+console.log(nomeItemsCarrinho)
+
+const qtdeXValor = qtdeXValor => qtdeXValor.qtde * qtdeXValor.preco
+const qtdeXValorCarrinho = carrinho.map(qtdeXValor)
+console.log(qtdeXValorCarrinho)
