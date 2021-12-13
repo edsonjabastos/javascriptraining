80dd8d2222018c4c649f3f3489eeda86595d4d95
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-13 15:13:39 -0300

    Serfrontend propriedade name

--- serfrontend/funcoes/arguments.js
+++ serfrontend/funcoes/arguments.js
@@ -1,5 +1,5 @@
 // const somar = () => {} // usando arrow functions não temos acesso ao arguments
-
+// toda função tem a propriedade name que retorna o nome da função
 const somar = function() {
     console.log(arguments)
 
@@ -12,6 +12,7 @@ const somar = function() {
 }
 // somar()
 
+console.log(somar.name)
 console.log(somar(0, 1, 2, 3, 4))
 console.log(somar(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
 console.log(somar(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13))
