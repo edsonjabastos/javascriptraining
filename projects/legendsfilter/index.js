9012f3b7e50c39fa1fce3879593bd9bf677657d9
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-14 08:37:39 -0300

    Project Legends Filter # Separando linhas

--- projects/legendsfilter/index.js
+++ projects/legendsfilter/index.js
@@ -6,4 +6,6 @@ const caminho = path.join(__dirname, "legendas");
 fn.lerDiretorio(caminho)
   .then((arquivos) => fn.elementosTerminadosCom(arquivos, ".srt"))
   .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+  .then(conteudos => conteudos.join('\n'))
+  .then(todConteudo => todConteudo.split('\n'))
   .then(console.log);
