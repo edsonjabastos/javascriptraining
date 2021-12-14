b40e9bd151696303559875beb7ff7c7f9c297d69
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-14 08:13:33 -0300

    Project Legends Filter # Filtrando arquivos por extensão

--- projects/legendsfilter/index.js
+++ projects/legendsfilter/index.js
@@ -4,4 +4,5 @@ const fn = require("./funcoes");
 const caminho = path.join(__dirname, "legendas");
 
 fn.lerDiretorio(caminho)
-    .then(console.log);
+  .then((arquivos) => fn.elementosTerminadosCom(arquivos, ".srt"))
+  .then(console.log);
