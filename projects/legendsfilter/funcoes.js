d55b999e1d5185f240eed3f96d053278a28c8762
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-14 09:02:29 -0300

    Project Legends Filter # Remover linhas com tempo

--- projects/legendsfilter/funcoes.js
+++ projects/legendsfilter/funcoes.js
@@ -13,8 +13,8 @@ function lerDiretorio(caminho) {
   });
 }
 
-function elementosTerminadosCom(array, padrao) {
-  return array.filter((el) => el.endsWith(padrao));
+function elementosTerminadosCom(array, padraoTextual) {
+  return array.filter((el) => el.endsWith(padraoTextual));
 }
 
 function lerArquivo(caminho) {
@@ -29,16 +29,21 @@ function lerArquivo(caminho) {
 }
 
 function lerArquivos(caminhos) {
-    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+  return Promise.all(caminhos.map((caminho) => lerArquivo(caminho)));
 }
 
 function removerSeVazio(array) {
-    return array.filter(el => el.trim())
+  return array.filter((el) => el.trim());
+}
+
+function removerSeIncluir(array, padraoTextual) {
+  return array.filter((el) => !el.includes(padraoTextual));
 }
 
 module.exports = {
   lerDiretorio,
   elementosTerminadosCom,
   lerArquivos,
-  removerSeVazio
+  removerSeVazio,
+  removerSeIncluir,
 };
