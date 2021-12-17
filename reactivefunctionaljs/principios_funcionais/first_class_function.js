bfd9581fd972e7f896c94dd1c9c7b87ed5a0a0ad
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-16 21:59:06 -0300

    FuncionalJS - first class function

---
+++ reactivefunctionaljs/principios_funcionais/first_class_function.js
@@ -0,0 +1,15 @@
+// diz-se que uma linguagem de programação possui
+// funções de primeira classe quando funções nessa
+// linguagem são tratadas como qualquer outra varável.
+
+const x = 3;
+
+const y = function (txt) {
+  return `esse é o texto => ${txt}`;
+};
+
+const z = () => console.log("ZZZZzzzZZZzzz");
+
+console.log(x);
+console.log(y("Eaaerr meu chapa"));
+z()
