5a2a9a84d63d6ae549517f713844cd4bb8593c08
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-29 08:17:41 -0300

    FuncionalJS - desafio composição de função

--- projects/legendsfilter/funcoes.js
+++ projects/legendsfilter/funcoes.js
@@ -96,7 +96,20 @@ function ordernarPorAtributoNumerico(atributo, ordem = "asc") {
   };
 }
 
+function composicao(...fns) {
+  return function (valor) {
+    return fns.reduce(async (acc, fn) => {
+      if (Promise.resolve(acc) === acc) {
+        return fn(await acc);
+      } else {
+        return fn(acc);
+      }
+    }, valor);
+  };
+}
+
 module.exports = {
+  composicao,
   lerDiretorio,
   elementosTerminadosCom,
   lerArquivos,
--- projects/legendsfilter/index.js
+++ projects/legendsfilter/index.js
@@ -21,19 +21,39 @@ const simbolos = [
   ")",
 ];
 
-fn.lerDiretorio(caminho)
-  .then(fn.elementosTerminadosCom(".srt"))
-  .then(fn.lerArquivos)
-  .then(fn.mesclarElementos)
-  .then(fn.separarTextoPor("\n"))
-  .then(fn.removerElementosSeVazio)
-  .then(fn.removerElementosSeIncluir("-->"))
-  .then(fn.removerElementosSeApenasNumero)
-  .then(fn.removerSimbolos(simbolos))
-  .then(fn.mesclarElementos)
-  .then(fn.separarTextoPor(" "))
-  .then(fn.removerElementosSeVazio)
-  .then(fn.removerElementosSeApenasNumero)
-  .then(fn.agruparElementos)
-  .then(fn.ordernarPorAtributoNumerico("qtd", "desc"))
-  .then(console.log);
+const palavrasMaisUsadas = fn.composicao(
+  fn.lerDiretorio,
+  fn.elementosTerminadosCom(".srt"),
+  fn.lerArquivos,
+  fn.mesclarElementos,
+  fn.separarTextoPor("\n"),
+  fn.removerElementosSeVazio,
+  fn.removerElementosSeIncluir("-->"),
+  fn.removerElementosSeApenasNumero,
+  fn.removerSimbolos(simbolos),
+  fn.mesclarElementos,
+  fn.separarTextoPor(" "),
+  fn.removerElementosSeVazio,
+  fn.removerElementosSeApenasNumero,
+  fn.agruparElementos,
+  fn.ordernarPorAtributoNumerico("qtd", "desc")
+);
+
+palavrasMaisUsadas(caminho).then(console.log)
+
+// fn.lerDiretorio(caminho)
+//   .then(fn.elementosTerminadosCom(".srt"))
+//   .then(fn.lerArquivos)
+//   .then(fn.mesclarElementos)
+//   .then(fn.separarTextoPor("\n"))
+//   .then(fn.removerElementosSeVazio)
+//   .then(fn.removerElementosSeIncluir("-->"))
+//   .then(fn.removerElementosSeApenasNumero)
+//   .then(fn.removerSimbolos(simbolos))
+//   .then(fn.mesclarElementos)
+//   .then(fn.separarTextoPor(" "))
+//   .then(fn.removerElementosSeVazio)
+//   .then(fn.removerElementosSeApenasNumero)
+//   .then(fn.agruparElementos)
+//   .then(fn.ordernarPorAtributoNumerico("qtd", "desc"))
+//   .then(console.log);
